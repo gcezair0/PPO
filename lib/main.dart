892f553393609b915f451edb717db77d0eff8a83
Guilherme Cezar <105322907+gcezair0@users.noreply.github.com>
2024-03-05 22:42:59 -0300

    Arquivos necessários]

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:pdv_ppo/src/pages/home_modulo/home_page.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+    return MaterialApp( // Adicionando MaterialApp
+      home: homePage(),
     );
   }
 }
+
+
+
 
 
